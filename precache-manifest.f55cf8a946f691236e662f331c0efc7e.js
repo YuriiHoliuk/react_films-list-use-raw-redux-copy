6a3a3bb95b3e2802ce1ba3dec8a35590671e8fee
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-10-09 07:54:49 +0300

    deploy 2

---
+++ precache-manifest.f55cf8a946f691236e662f331c0efc7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7daf5d3385cb77695db7047a075afe4e",
+    "url": "/react_films-list-use-raw-redux-copy/index.html"
+  },
+  {
+    "revision": "8758c89e2062f8a6f229",
+    "url": "/react_films-list-use-raw-redux-copy/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "8be95da67b29af226a90",
+    "url": "/react_films-list-use-raw-redux-copy/static/css/main.7fe3b127.chunk.css"
+  },
+  {
+    "revision": "8758c89e2062f8a6f229",
+    "url": "/react_films-list-use-raw-redux-copy/static/js/2.bc69c30a.chunk.js"
+  },
+  {
+    "revision": "8be95da67b29af226a90",
+    "url": "/react_films-list-use-raw-redux-copy/static/js/main.03298d58.chunk.js"
+  },
+  {
+    "revision": "89fcf94e9304d8a68bbd",
+    "url": "/react_films-list-use-raw-redux-copy/static/js/runtime~main.f3dec76c.js"
+  }
+]);
